69b4d19f75f5791c6ea4b2863d45922bf6ccd750
codeawy <codeawy@gmail.com>
2023-10-05 08:43:56 +0300

    starter

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen p-24">
+      <h1 className="text-center text-4xl bg-white rounded-md text-black w-fit p-5 mx-auto font-semibold">
+        Design System in Depth
+      </h1>
+    </main>
+  );
+}
